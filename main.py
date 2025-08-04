2e1de1704da02a459fb4cac1ca02e8d8c1fb8b27
Abain-1 <abain.1x@gmail.com>
2025-08-03 23:17:19 -0400

    added getbooktext

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def main():
+
+    def get_book_text(path_to_file):
+        with open(path_to_file) as f:
+            file_contents = f.read()
+
+        print(file_contents)
+
+    get_book_text("./books/frankenstein.txt")
+
+
+main()
+
